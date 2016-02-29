2623b0cc4fa3e7a7b1e48ab3ac3313df19d0fdb2
sutingwei <409996170@qq.com>
2016-02-29 15:53:46 +0800

    first commit

---
+++ Assets/Script/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Enemy generic behavior
+/// </summary>
+public class EnemyScript : MonoBehaviour
+{
+    private WeaponScript weapon;
+
+    void Awake()
+    {
+        // Retrieve the weapon only once
+        weapon = GetComponentInChildren<WeaponScript>();
+    }
+
+    void Update()
+    {
+        if (GetComponent<Renderer>().IsVisibleFrom(Camera.main) == false)
+            return;
+        // Auto-fire
+        if (weapon != null && weapon.CanAttack)
+        {
+            weapon.Attack(true);
+            SoundEffectsHelper.Instance.MakeEnemyShotSound();
+        }
+    }
+}
